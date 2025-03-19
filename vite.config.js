72705aa9500f7e68c3dc6f926fc6ee1087942381
Samuel Souza <souzacarvalhosamuel@gmail.com>
2025-03-18 23:05:26 -0300

    Ajustado build para funcionar temas
    - Modificado o caminho usado para importar os temas no index.html.
    - Ajustado o vite para criar a build no projeto raiz

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 export default {
-    root: "public",
-    build: {
-      outDir: "../dist",
-      emptyOutDir: true,
+  root: "public", // 🔹 Mantém a pasta "public" como raiz do projeto
+  build: {
+    outDir: "../dist", // 🔹 Salva a build fora de "public/", diretamente na raiz do projeto
+    emptyOutDir: true, // 🔹 Apaga arquivos antigos antes de criar novos
+  },
+  publicDir: "../public", // 🔹 Mantém os arquivos estáticos de "public/" acessíveis
+  server: {
+    watch: {
+      usePolling: true,
     },
-    server: {
-      watch: {
-        usePolling: true,
-      },
-      open: true,
-    },
-  };
-  
+    open: true,
+  },
+};
